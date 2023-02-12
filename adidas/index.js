8f370182ddb78aec927ba13e5b4ad20b43dc09f3
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-02-12 18:21:08 +0200

    Aded new studying elements

---
+++ adidas/index.js
@@ -0,0 +1,12 @@
+// function printNumbers(from, to) {
+//     // let timerId = setTimeout(function tick() {
+//     //     if (from <= to) {
+//     //         alert(from++);
+//     //         timerId = setTimeout(tick, 1000);
+//     //     } else {
+//     //         clearTimeout(timerId);
+//     //     }
+//     // }, 1000);
+// }
+
+// printNumbers(2, 5);
