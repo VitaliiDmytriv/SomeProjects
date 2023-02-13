7a41ca8700710680921ef4d00cb21503ea908331
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-02-13 10:11:28 +0200

    Learn and add caching

--- adidas/index.js
+++ adidas/index.js
@@ -10,3 +10,31 @@
 // }
 
 // printNumbers(2, 5);
+
+// ========================================= Кешування!
+
+function multiplic(x) {
+    return Math.pow(x, 3);
+}
+
+function cachingDecorator(func) {
+    let cache = new Map();
+
+    return function (x) {
+        if (cache.has(x)) {
+            console.log("get from cache");
+            return cache.get(x);
+        }
+
+        let result = func(x);
+        console.log("get from function calculation");
+        cache.set(x, result);
+        return result;
+    };
+}
+
+let getRes = cachingDecorator(multiplic);
+
+console.log(getRes(2));
+console.log(getRes(5));
+console.log(getRes(5));
