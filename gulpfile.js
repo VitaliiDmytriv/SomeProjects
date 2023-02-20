cf3689e5ba15d92ef63de401446126b252fee316
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-02-20 17:44:12 +0200

    add gulp and change root file css

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const watch = require("gulp-watch");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+
+gulp.task("sass", function () {
+    return gulp
+        .src("./Responsive_learning/style/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(sourcemaps.write("./"))
+        .pipe(concat("main.css"))
+        .pipe(gulp.dest("./Responsive_learning/"));
+});
+
+gulp.task("run", function () {
+    gulp.watch("./src/style/**/*.scss", gulp.series("sass"));
+});
