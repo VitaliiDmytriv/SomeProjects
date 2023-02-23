322f3d3cad27487eaf902e649cc8048789fa6a16
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-02-23 20:02:03 +0200

    done for 23.02

--- Responsive_learning/index.js
+++ Responsive_learning/index.js
@@ -1,6 +1,7 @@
 const inlineLinesStroke = document.querySelectorAll(".line__inline-stroke");
 const lineBlockStroke = document.querySelector(".line__block-stroke");
 const lineContainer = document.querySelector(".line");
+const circle = document.querySelectorAll(".line__circle");
 
 //vatiables of container
 const lineContainerWidth = lineContainer.getBoundingClientRect().width;
@@ -8,75 +9,95 @@ const inlineLinesStrokeWith = lineContainerWidth / 2 - 13;
 
 const card = document.querySelectorAll(".roadmap__card");
 
-window.addEventListener("scroll", () => {
-    const { scrollTop, clientHeight } = this.document.documentElement;
-    let gap = card[0].getBoundingClientRect().height;
-    // console.log(clientHeight - lineBlockStroke.getBoundingClientRect().top - 519);
+if (inlineLinesStroke[5].getBoundingClientRect().width === inlineLinesStrokeWith) {
+    console.log(true);
+}
 
-    // width
-    if (clientHeight - inlineLinesStroke[0].getBoundingClientRect().top - 180 > 0) {
-        let width = clientHeight - inlineLinesStroke[0].getBoundingClientRect().top - 180;
-        inlineLinesStroke[0].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
-    }
+window.addEventListener("scroll", function sizes() {
+    if (inlineLinesStroke[5].getBoundingClientRect().width !== inlineLinesStrokeWith) {
+        const { scrollTop, clientHeight } = this.document.documentElement;
+        let gap = card[0].getBoundingClientRect().height;
+        // console.log(clientHeight - lineBlockStroke.getBoundingClientRect().top - 519);
 
-    // height
-    if (inlineLinesStroke[0].getBoundingClientRect().width === inlineLinesStrokeWith) {
-        let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 255;
-        lineBlockStroke.style.height = `${Math.min(width * 1.3, gap)}px`;
-    }
+        // width
+        if (clientHeight - inlineLinesStroke[0].getBoundingClientRect().top - 180 > 0) {
+            let width = clientHeight - inlineLinesStroke[0].getBoundingClientRect().top - 180;
+            inlineLinesStroke[0].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
+        }
 
-    // width
-    if (lineBlockStroke.getBoundingClientRect().height === gap) {
-        let width = clientHeight - inlineLinesStroke[1].getBoundingClientRect().top - 217;
-        inlineLinesStroke[1].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
-    }
+        // height
+        if (inlineLinesStroke[0].getBoundingClientRect().width === inlineLinesStrokeWith) {
+            card[0].classList.add("active");
+            let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 255;
+            lineBlockStroke.style.height = `${Math.min(width * 1.3, gap)}px`;
+        }
 
-    // height
-    if (inlineLinesStroke[1].getBoundingClientRect().width === inlineLinesStrokeWith) {
-        let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 351;
-        lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 2)}px`;
-    }
+        // width
+        if (lineBlockStroke.getBoundingClientRect().height === gap) {
+            circle[1].classList.add("active");
+            let width = clientHeight - inlineLinesStroke[1].getBoundingClientRect().top - 217;
+            inlineLinesStroke[1].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
+        }
 
-    // width
-    if (lineBlockStroke.getBoundingClientRect().height === gap * 2) {
-        let width = clientHeight - inlineLinesStroke[2].getBoundingClientRect().top - 265;
-        inlineLinesStroke[2].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
-    }
+        // height
+        if (inlineLinesStroke[1].getBoundingClientRect().width === inlineLinesStrokeWith) {
+            card[1].classList.add("active");
+            let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 351;
+            lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 2)}px`;
+        }
 
-    // height
-    if (inlineLinesStroke[2].getBoundingClientRect().width === inlineLinesStrokeWith) {
-        let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 444;
-        lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 3)}px`;
-    }
+        // width
+        if (lineBlockStroke.getBoundingClientRect().height === gap * 2) {
+            circle[2].classList.add("active");
+            let width = clientHeight - inlineLinesStroke[2].getBoundingClientRect().top - 265;
+            inlineLinesStroke[2].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
+        }
 
-    // width
-    if (Math.ceil(lineBlockStroke.getBoundingClientRect().height) === Math.ceil(gap * 3)) {
-        let width = clientHeight - inlineLinesStroke[3].getBoundingClientRect().top - 325;
-        inlineLinesStroke[3].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
-    }
+        // height
+        if (inlineLinesStroke[2].getBoundingClientRect().width === inlineLinesStrokeWith) {
+            card[2].classList.add("active");
+            let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 444;
+            lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 3)}px`;
+        }
 
-    // height
-    if (inlineLinesStroke[3].getBoundingClientRect().width === inlineLinesStrokeWith) {
-        let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 537;
-        lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 4)}px`;
-    }
+        // width
+        if (Math.ceil(lineBlockStroke.getBoundingClientRect().height) === Math.ceil(gap * 3)) {
+            circle[3].classList.add("active");
+            let width = clientHeight - inlineLinesStroke[3].getBoundingClientRect().top - 325;
+            inlineLinesStroke[3].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
+        }
 
-    // width
-    if (Math.ceil(lineBlockStroke.getBoundingClientRect().height) === Math.ceil(gap * 4)) {
-        let width = clientHeight - inlineLinesStroke[4].getBoundingClientRect().top - 385;
-        inlineLinesStroke[4].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
-    }
+        // height
+        if (inlineLinesStroke[3].getBoundingClientRect().width === inlineLinesStrokeWith) {
+            card[3].classList.add("active");
+            let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 537;
+            lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 4)}px`;
+        }
 
-    // height
-    if (inlineLinesStroke[4].getBoundingClientRect().width === inlineLinesStrokeWith) {
-        let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 640;
-        lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 5)}px`;
-    }
+        // width
+        if (Math.ceil(lineBlockStroke.getBoundingClientRect().height) === Math.ceil(gap * 4)) {
+            circle[4].classList.add("active");
+            let width = clientHeight - inlineLinesStroke[4].getBoundingClientRect().top - 385;
+            inlineLinesStroke[4].style.width = `${Math.min(width * 1.3, inlineLinesStrokeWith)}px`;
+        }
 
-    // width
-    if (Math.ceil(lineBlockStroke.getBoundingClientRect().height) <= Math.ceil(gap * 5)) {
-        console.log(true);
-        let width = clientHeight - inlineLinesStroke[5].getBoundingClientRect().top - 455;
-        inlineLinesStroke[5].style.width = `${Math.min(width * 1.8, inlineLinesStrokeWith)}px`;
+        // height
+        if (inlineLinesStroke[4].getBoundingClientRect().width === inlineLinesStrokeWith) {
+            card[4].classList.add("active");
+            let width = clientHeight - lineBlockStroke.getBoundingClientRect().top - 640;
+            lineBlockStroke.style.height = `${Math.min(width * 1.3, gap * 5)}px`;
+        }
+
+        // width
+        if (lineBlockStroke.getBoundingClientRect().height == Math.floor(gap * 5)) {
+            circle[5].classList.add("active");
+            let width = clientHeight - inlineLinesStroke[5].getBoundingClientRect().top - 455;
+            inlineLinesStroke[5].style.width = `${Math.min(width * 1.8, inlineLinesStrokeWith)}px`;
+        }
+        if (inlineLinesStroke[5].getBoundingClientRect().width === inlineLinesStrokeWith) {
+            card[5].classList.add("active");
+        }
+    } else {
+        return null;
     }
 });
