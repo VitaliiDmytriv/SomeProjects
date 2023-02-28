0c71f9b054c3d10326ddbd175bde668b8bcd06ee
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-02-28 16:01:26 +0200

    Done with styling footer, need to fix dropdown

--- Responsive_learning/app.js
+++ Responsive_learning/app.js
@@ -1,40 +1,48 @@
-new Swiper(".feedback-slider", {
-    loop: true,
-    speed: 900,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 4000,
-        // pauseOnMouseEnter: true,
-    },
-});
-
-new Swiper(".blog__slider", {
-    spaceBetween: 30,
-    initialSlide: 1,
-    loop: true,
-    slideToClickedSlide: true,
-    breakpoints: {
-        593: {
-            centeredSlides: true,
-            slidesPerView: 2,
-            spaceBetween: 30,
-            loop: false,
+$(document).ready(function () {
+    new Swiper(".feedback-slider", {
+        loop: true,
+        speed: 900,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true,
         },
-
-        1000: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-            centeredSlides: true,
-            loop: false,
+        autoplay: {
+            delay: 4000,
+            // pauseOnMouseEnter: true,
         },
-    },
-    navigation: {
-        nextEl: ".blog__nextBtn",
-        prevEl: ".blog__prevBtn",
-    },
+    });
+
+    new Swiper(".blog__slider", {
+        spaceBetween: 30,
+        initialSlide: 1,
+        loop: true,
+        slideToClickedSlide: true,
+        breakpoints: {
+            593: {
+                centeredSlides: true,
+                slidesPerView: 2,
+                spaceBetween: 30,
+                loop: false,
+            },
+
+            1000: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                centeredSlides: true,
+                loop: false,
+            },
+        },
+        navigation: {
+            nextEl: ".blog__nextBtn",
+            prevEl: ".blog__prevBtn",
+        },
+    });
+
+    // $(".dropdown-triger").click(function (e) {
+    //     e.preventDefault();
+    //     // $(".dropdown-menu").slideDown();
+    //     console.log(e);
+    // });
 });
